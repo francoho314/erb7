a26ce6501e4ba190486d1dd53971975f4a91d327
franco <francoho68@gmail.com>
2025-10-02 17:53:11 +0800

    step 10 : listing template

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     "debug_toolbar",
+    
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'doctors.apps.DoctorsConfig',
